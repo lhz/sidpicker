3d8b0fb467ad8599b52403fe3b046597e072b1e0
Lars Haugseth <lars.haugseth@amedia.no>
2016-12-21 15:59:42 +0100

    Started work on XML release parsing.

--- hvsc/releases.go
+++ hvsc/releases.go
@@ -8,6 +8,9 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+
+	"github.com/beevik/etree"
 )
 
 const ReleasesUsedFile = "releases.json.gz"
@@ -16,7 +19,7 @@ type Release struct {
 	Id    int    `json:"id"`
 	Name  string `json:"name"`
 	Group string `json:"group"`
-	Year  string `json:"year"`
+	Date  string `json:"date"`
 }
 
 func (r *Release) URL() string {
@@ -54,4 +57,45 @@ func readReleases() {
 		}
 		Tunes[tuneIndex].Releases = releases
 	}
+}
+
+func readReleaseXML(path) {
+	doc := etree.NewDocument()
+	err := doc.ReadFromFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tunePaths := doc.FindElements("/CSDbData/Release/UsedSIDs/SID/HVSCPath")
+	if len(tunePaths) < 1 {
+		return
+	}
+
+	rel := doc.SelectElement("CSDbData/Release")
+
+}
+
+func parseRelease(e *etree.Element) *Release {
+	var err error
+	r := Release{}
+	r.Id, err = strconv.Atoi(e.SelectElement("ID").Text())
+	r.Name = e.SelectElement("Name").Text()
+	r.Type = e.SelectElement("Type").Text()
+	date := bytes.Buffer{}
+	y := e.SelectElement("ReleaseYear")
+	if y != nil {
+		date.WriteString(y.Text())
+	}
+	m := e.SelectElement("ReleaseMonth")
+	if m != nil {
+		date.WriteString(fmt.Sprintf("-%02s", m.Text()))
+	} else {
+		date.WriteString("-xx")
+	}
+	d := e.SelectElement("ReleaseDay")
+	if d != nil {
+		date.WriteString(fmt.Sprintf("-%02s", d.Text()))
+	} else {
+		date.WriteString("-xx")
+	}
+	r.Date = date.String()
 }
